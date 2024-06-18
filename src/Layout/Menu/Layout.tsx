a61164969f2aca5e5922f2bc385baf46ff9b5647
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-06-18 22:20:15 +0300

    adds use of useLocation

--- src/Layout/Menu/Layout.tsx
+++ src/Layout/Menu/Layout.tsx
@@ -1,9 +1,16 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import styles from './Layout.module.css'
 import clsx from "clsx";
+import { useEffect } from "react";
 
 export function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log(location)
+    }, [location]);
+
     return (
         <div className={styles['layout']}>
             <div className={styles['sidebar']}>
@@ -13,8 +20,12 @@ export function Layout() {
                     <p className={styles['client-email']}>fantastic@fox.ru</p>
                 </div>
                 <nav className={styles['menu']}>
-                    <Link className={clsx(styles['link'], styles['menu-icon'])} to='/'>Меню</Link>
-                    <Link className={clsx(styles['link'], styles['cart-icon'])} to='/cart'>Корзина</Link>
+                    <Link className={clsx(styles['link'], styles['menu-icon'],
+                        { [styles.active]: location.pathname === '/' }
+                    )} to='/'>Меню</Link>
+                    <Link className={clsx(styles['link'], styles['cart-icon'],
+                        { [styles.active]: location.pathname === '/cart' }
+                    )} to='/cart'>Корзина</Link>
                 </nav>
                 <Button className={styles['exit']}>
                     <img className={styles['exit-icon']} src='public/exit-icon.svg' alt='Exit' />
