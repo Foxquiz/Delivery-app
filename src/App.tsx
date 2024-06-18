47e0cd3e5bf7a4b05958801545934b12bf27b55c
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-06-18 17:29:24 +0300

    changes routes ex on new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import { Menu } from "./pages/Menu/Menu";
 import { Cart } from "./pages/Cart/Cart";
 import { Error } from "./pages/Error/Error";
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '*',
+    element: <Error />
+  }
+]);
 
 function App() {
 
@@ -23,11 +36,7 @@ function App() {
         <a href='/'>Меню</a>
         <a href='/cart'>Корзина</a>
       </div>
-      <Routes>
-        <Route path='/' element={<Menu/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='*' element={<Error/>}/>
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
