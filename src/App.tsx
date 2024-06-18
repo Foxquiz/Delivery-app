6b05e44b0a50878c907039bdadbf9035d3448599
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-06-18 17:00:27 +0300

    adds react-router with example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Route, Routes } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
+
 
 
 function App() {
@@ -13,9 +18,18 @@ function App() {
         Применить
       </Button>
       <Input placeholder="Email" />
-      <Input placeholder="Promo" />
+      <Input placeholder="Password" />
+      <div>
+        <a href='/'>Меню</a>
+        <a href='/cart'>Корзина</a>
+      </div>
+      <Routes>
+        <Route path='/' element={<Menu/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
