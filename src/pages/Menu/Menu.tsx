74b843984519e84a455f25b340a854bc431a7640
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-06-26 01:03:22 +0300

    adds search filter rev1

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -3,9 +3,13 @@ import { HeaderTitle } from "../../components/HeaderTitle/HeaderTitle";
 import styles from './Menu.module.css'
 import { PREFIX } from "../../helpers/API";
 import { Product } from "../../interfaces/product.interface";
-import { useEffect, useState } from "react";
+import { FormEventHandler, useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
 import { MenuList } from "./MenuList/MenuList";
+
+export interface Search extends HTMLInputElement {
+    value: string;
+}
 
 export function Menu() {
     const [products, setProducts] = useState<Product[]>([]);
@@ -13,10 +17,14 @@ export function Menu() {
     const [error, setError] = useState<string | undefined>();
 
 
-    const getMenu = async () => {
+    const getMenu = async (searchString = '') => {
         try {
             setIsLoading(true);
-            const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
+            let request = 'products';
+            if (searchString) {
+                request = `products?name=${searchString}`
+            }
+            const { data } = await axios.get<Product[]>(`${PREFIX}/${request}`);
             setProducts(data);
             setIsLoading(false);
         } catch (e) {
@@ -33,11 +41,16 @@ export function Menu() {
         getMenu();
     }, [])
 
+    const search: FormEventHandler<HTMLInputElement> = (e) => {
+        const target = e.target as typeof e.target & Search;
+        getMenu(target.value);
+    }
+
     return (
         <>
             <header className={styles['header']}>
                 <HeaderTitle>Меню</HeaderTitle>
-                <SearchInput placeholder="Введите блюдо или состав" />
+                <SearchInput name='search' placeholder="Введите блюдо или состав" onInput={search} />
             </header>
             <div className={styles['content']}>
                 {!isLoading && <MenuList products={products} />}
